8138c219ee1f891a2d377fc968e7506078fae6bb
Ivo Gregorio <idgrego@msn.com>
2025-12-08 21:16:06 -0300

    middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,17 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ArithmeticModule } from './arithmetic/arithmetic.module';
+import { LogMiddleware } from './mid/log/log.middleware';
 
 @Module({
   imports: [ArithmeticModule],
   controllers: [AppController],
   providers: [AppService],
 })
-// eslint-disable-next-line prettier/prettier
-export class AppModule { }
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    //consumer.apply(LogMiddleware).forRoutes('*');
+    consumer.apply(LogMiddleware).forRoutes('arithmetic');
+  }
+}
